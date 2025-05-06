970d3fff2f28a7c35cba1c6fd63f73ee432fd2f3
richard <richarda.tyarks@hes-so.ch>
2025-05-06 10:20:56 +0200

    fix: change CreateArticleInGraph fonction

--- graph/create_graph.go
+++ graph/create_graph.go
@@ -8,66 +8,59 @@ import (
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 )
 
-func CreateGraphFromArticles(ctx context.Context, session neo4j.SessionWithContext, articles []models.Article) {
-	for _, article := range articles {
-		// ARTICLE node
+func CreateArticleInGraph(ctx context.Context, session neo4j.SessionWithContext, article models.Article) {
+	_, err := session.ExecuteWrite(ctx, func(tx neo4j.ManagedTransaction) (any, error) {
+		query := `MERGE (a:Article {_id: $id}) SET a.title = $title`
+		params := map[string]any{
+			"id":    article.ID,
+			"title": article.Title,
+		}
+		_, err := tx.Run(ctx, query, params)
+		return nil, err
+	})
+	if err != nil {
+		log.Printf("Article %s: %v", article.ID, err)
+		return
+	}
+
+	for _, author := range article.Authors {
 		_, err := session.ExecuteWrite(ctx, func(tx neo4j.ManagedTransaction) (any, error) {
-			query := `MERGE (a:Article {_id: $id}) SET a.title = $title`
+			query := `
+				MERGE (au:Author {_id: $authorId})
+				SET au.name = $name
+				WITH au
+				MATCH (ar:Article {_id: $articleId})
+				MERGE (au)-[:AUTHORED]->(ar)
+			`
 			params := map[string]any{
-				"id":    article.ID,
-				"title": article.Title,
+				"authorId":  author.ID,
+				"name":      author.Name,
+				"articleId": article.ID,
 			}
 			_, err := tx.Run(ctx, query, params)
 			return nil, err
 		})
 		if err != nil {
-			log.Printf("Article %s: %v", article.ID, err)
-			continue
-		}
-
-		// AUTHORS + AUTHORED
-		for _, author := range article.Authors {
-			_, err := session.ExecuteWrite(ctx, func(tx neo4j.ManagedTransaction) (any, error) {
-				query := `
-                    MERGE (au:Author {_id: $authorId})
-                    SET au.name = $name
-                    WITH au
-                    MATCH (ar:Article {_id: $articleId})
-                    MERGE (au)-[:AUTHORED]->(ar)
-                `
-				params := map[string]any{
-					"authorId":  author.ID,
-					"name":      author.Name,
-					"articleId": article.ID,
-				}
-				_, err := tx.Run(ctx, query, params)
-				return nil, err
-			})
-			if err != nil {
-				log.Printf("Author %s: %v", author.ID, err)
-			}
-		}
-
-		// CITES relationships
-		for _, refId := range article.References {
-			_, err := session.ExecuteWrite(ctx, func(tx neo4j.ManagedTransaction) (any, error) {
-				query := `
-                    MATCH (src:Article {_id: $srcId})
-                    MERGE (tgt:Article {_id: $tgtId})
-                    MERGE (src)-[:CITES]->(tgt)
-                `
-				params := map[string]any{
-					"srcId": article.ID,
-					"tgtId": refId,
-				}
-				_, err := tx.Run(ctx, query, params)
-				return nil, err
-			})
-			if err != nil {
-				log.Printf("CITES %s -> %s: %v", article.ID, refId, err)
-			}
+			log.Printf("Author %s: %v", author.ID, err)
 		}
 	}
 
-	log.Println("Graph creation completed.")
+	for _, refId := range article.References {
+		_, err := session.ExecuteWrite(ctx, func(tx neo4j.ManagedTransaction) (any, error) {
+			query := `
+				MATCH (src:Article {_id: $srcId})
+				MERGE (tgt:Article {_id: $tgtId})
+				MERGE (src)-[:CITES]->(tgt)
+			`
+			params := map[string]any{
+				"srcId": article.ID,
+				"tgtId": refId,
+			}
+			_, err := tx.Run(ctx, query, params)
+			return nil, err
+		})
+		if err != nil {
+			log.Printf("CITES %s -> %s: %v", article.ID, refId, err)
+		}
+	}
 }
