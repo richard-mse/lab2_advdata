83f5f977ac3ec848cdb0afdbee84994519f9f530
richard <richarda.tyarks@hes-so.ch>
2025-05-19 22:28:09 +0200

    opti cypher request 1

--- graph/create_graph.go
+++ graph/create_graph.go
@@ -3,64 +3,49 @@ package graph
 import (
 	"context"
 	"lab2-advdata/models"
-	"log"
 
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 )
 
-func CreateArticleInGraph(ctx context.Context, session neo4j.SessionWithContext, article models.Article) {
+// CreateArticleInGraph insère/met à jour un Article, ses Authors et ses References
+// dans une seule transaction Cypher optimisée.
+func CreateArticleInGraph(ctx context.Context, session neo4j.SessionWithContext, article models.Article) error {
+	// Préparer la liste d'auteurs pour le paramètre UNWIND
+	authorsParam := make([]map[string]any, len(article.Authors))
+	for i, au := range article.Authors {
+		authorsParam[i] = map[string]any{
+			"id":   au.ID,
+			"name": au.Name,
+		}
+	}
+
+	// Cypher unique
+	const query = `
+MERGE (a:Article {_id: $id})
+SET   a.title = $title
+
+WITH a
+UNWIND $authors AS aut
+  MERGE (au:Author {_id: aut.id})
+  SET   au.name = aut.name
+  MERGE (au)-[:AUTHORED]->(a)
+
+WITH a
+UNWIND $refs AS refId
+  MERGE (r:Article {_id: refId})
+  MERGE (a)-[:CITES]->(r)
+`
+
+	params := map[string]any{
+		"id":      article.ID,
+		"title":   article.Title,
+		"authors": authorsParam,
+		"refs":    article.References,
+	}
+
+	// Un seul ExecuteWrite pour tout
 	_, err := session.ExecuteWrite(ctx, func(tx neo4j.ManagedTransaction) (any, error) {
-		query := `MERGE (a:Article {_id: $id}) SET a.title = $title`
-		params := map[string]any{
-			"id":    article.ID,
-			"title": article.Title,
-		}
-		_, err := tx.Run(ctx, query, params)
-		return nil, err
+		return tx.Run(ctx, query, params)
 	})
-	if err != nil {
-		log.Printf("Article %s: %v", article.ID, err)
-		return
-	}
-
-	for _, author := range article.Authors {
-		_, err := session.ExecuteWrite(ctx, func(tx neo4j.ManagedTransaction) (any, error) {
-			query := `
-				MERGE (au:Author {_id: $authorId})
-				SET au.name = $name
-				WITH au
-				MATCH (ar:Article {_id: $articleId})
-				MERGE (au)-[:AUTHORED]->(ar)
-			`
-			params := map[string]any{
-				"authorId":  author.ID,
-				"name":      author.Name,
-				"articleId": article.ID,
-			}
-			_, err := tx.Run(ctx, query, params)
-			return nil, err
-		})
-		if err != nil {
-			log.Printf("Author %s: %v", author.ID, err)
-		}
-	}
-
-	for _, refId := range article.References {
-		_, err := session.ExecuteWrite(ctx, func(tx neo4j.ManagedTransaction) (any, error) {
-			query := `
-				MATCH (src:Article {_id: $srcId})
-				MERGE (tgt:Article {_id: $tgtId})
-				MERGE (src)-[:CITES]->(tgt)
-			`
-			params := map[string]any{
-				"srcId": article.ID,
-				"tgtId": refId,
-			}
-			_, err := tx.Run(ctx, query, params)
-			return nil, err
-		})
-		if err != nil {
-			log.Printf("CITES %s -> %s: %v", article.ID, refId, err)
-		}
-	}
+	return err
 }
